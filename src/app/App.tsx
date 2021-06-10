53193ff48e14d566af7ff5917813a1cf8458d285
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-10 10:54:37 +0200

    :bug: Fix Tailwindcss import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,10 @@ function App(): JSX.Element {
           <img src={logo} className={styles['App-logo']} alt="logo" />
           <p>Hello Vite + React!</p>
           <p>
-            <button onClick={() => setCount((count) => count + 1)}>
+            <button
+              className="bg-green-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => setCount((count) => count + 1)}
+            >
               count is: {count}
             </button>
           </p>
