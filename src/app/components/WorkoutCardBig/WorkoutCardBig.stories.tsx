5c9f0e4ed5b665c6067642dd2488c59c3e34a5c8
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-10 18:18:46 +0200

    :sparkles: Add WorkoutCardBig to storybook

---
+++ src/app/components/WorkoutCardBig/WorkoutCardBig.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import WorkoutCardBig from './WorkoutCardBig';
+
+export default {
+  title: 'Components/WorkoutCardBig',
+  component: WorkoutCardBig,
+};
+
+export const Default = (): JSX.Element => <WorkoutCardBig />;
