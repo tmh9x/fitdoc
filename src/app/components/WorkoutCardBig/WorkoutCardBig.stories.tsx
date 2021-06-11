3cfacbffe96876d0ebc92ce5ca3ceb2e2500726a
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-11 14:49:31 +0200

    :recycle: Add props & rename file

--- src/app/components/WorkoutCardBig/WorkoutCardBig.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import WorkoutCardBig from './WorkoutCardBig';
-
-export default {
-  title: 'Components/WorkoutCardBig',
-  component: WorkoutCardBig,
-};
-
-export const Default = (): JSX.Element => <WorkoutCardBig />;
