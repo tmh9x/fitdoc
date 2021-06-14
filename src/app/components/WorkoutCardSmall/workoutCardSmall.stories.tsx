433e724f8f51afcb6a48f220f0335959f27ce072
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-14 11:06:48 +0200

    :sparkles: Add workourCardSmall to stories

---
+++ src/app/components/WorkoutCardSmall/workoutCardSmall.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import WorkoutCardSmall from './workoutCardSmall';
+
+export default {
+  title: 'Components/WorkoutCardSmall',
+  component: WorkoutCardSmall,
+};
+
+export const Card = (): JSX.Element => (
+  <WorkoutCardSmall
+    thumbnail="/pushups.jpeg"
+    name="Chest Workout"
+    category="Muscle"
+    iconTimer="/timer.png"
+    duration="15 min."
+    iconLevel="/level.png"
+    level="level"
+  />
+);
