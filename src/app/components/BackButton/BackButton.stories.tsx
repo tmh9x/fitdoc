2dadc122c13662949712b46a0484219496b850de
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-15 11:38:39 +0200

    ✨ Add BackButton stories

---
+++ src/app/components/BackButton/BackButton.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BackButton from './BackButton';
+
+export default {
+  title: 'Components/BackButton',
+  component: BackButton,
+};
+
+export const Card = (): JSX.Element => <BackButton thumbnail="back.png" />;
