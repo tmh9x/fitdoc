4aa91a42fa36c188b314a84e9f82029ef9a37363
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-15 15:10:26 +0200

    💄 Update font size

--- src/app/components/WorkoutCardOverview/WorkoutCardOverview.tsx
+++ src/app/components/WorkoutCardOverview/WorkoutCardOverview.tsx
@@ -15,7 +15,7 @@ function WorkoutCardOverview({
   return (
     <article className="bg-secondary w-80 rounded-xl grid auto-rows-min">
       <img className="rounded-t-xl" src={thumbnail} alt="" />
-      <h1 className="font-bold pt-6 px-4">{name}</h1>
+      <h1 className="font-bold pt-6 px-4 text-xl">{name}</h1>
       <p className="pt-2 px-4">{description}</p>
       <a className="text-secondary w-64 pt-2 pb-2 text-center place-self-center mt-4 mb-4 border bg-buttonVar1 rounded-xl">
         START WORKOUT
