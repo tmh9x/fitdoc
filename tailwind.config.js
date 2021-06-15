360f2a3a0973991bf509aa379966aea9eeece47f
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-15 15:04:42 +0200

    🔧 Update tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    textColor: {
+      primary: '#24272f;',
+      secondary: '#fff',
+    },
+    backgroundColor: {
+      primary: '#24272f;',
+      secondary: '#fff',
+      buttonVar1: '#ff2e55',
+    },
     extend: {},
     fontFamily: {
       sans: ['Roboto'],
