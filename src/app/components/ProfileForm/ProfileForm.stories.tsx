0c82f561b8a59a2f50bd3ba4364ddaed8f93f05c
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-18 17:35:32 +0200

    ✨ Add ProfileForm stories

---
+++ src/app/components/ProfileForm/ProfileForm.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ProfileForm from './ProfileForm';
+
+export default {
+  title: 'Components/ProfileForm',
+  component: ProfileForm,
+};
+
+export const Inputfield = (): JSX.Element => <ProfileForm />;
