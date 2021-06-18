e4fc7ec0c14f5d498acd56a11fc41936ea89dc12
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-18 10:19:55 +0200

    🚑️ Set configurations to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    textColor: {
-      primary: '#24272f;',
-      secondary: '#fff',
-    },
-    backgroundColor: {
-      primary: '#24272f;',
-      secondary: '#fff',
-      tertiary: '#707070',
-      primaryButton: '#ff2e55',
-      secondaryButton: '#43BA7C',
-    },
-    borderColor: {
-      primary: '#707070',
-    },
-    extend: {},
-    fontFamily: {
-      sans: ['Roboto'],
+    extend: {
+      textColor: {
+        primary: '#24272f;',
+        secondary: '#fff',
+      },
+      backgroundColor: {
+        primary: '#24272f;',
+        secondary: '#fff',
+        tertiary: '#707070',
+        primaryButton: '#ff2e55',
+        secondaryButton: '#43BA7C',
+      },
+      borderColor: {
+        primary: '#707070',
+      },
+      fontFamily: {
+        sans: ['Roboto'],
+      },
     },
   },
   variants: {
