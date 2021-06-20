0664c727717c12e3c70b8ce492af32e624e5a3b4
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-20 18:49:47 +0200

    ✨ Add MessageCard component

---
+++ src/app/components/MessageCard/MessageCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import Button from '../Button/Button';
+
+type MessageCardProps = {
+  thumbnail: string;
+  message: string;
+};
+
+function MessageCard({ thumbnail, message }: MessageCardProps): JSX.Element {
+  return (
+    <div className="p-6 mx-auto text-center w-80 text-secondary rounded-xl bg-primary">
+      <img className="w-40 mx-auto" src={thumbnail} alt="" />
+      <div className="p-6 text-xl">{message}</div>
+      <div>
+        <Button children="CONTINUE" variant="secondary"></Button>
+      </div>
+    </div>
+  );
+}
+
+export default MessageCard;
