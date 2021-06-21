1f4abc73a6d81a71489ef9bc3ab325f97e80a72d
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-21 21:25:56 +0200

    ✨ Add server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import path from 'path';
+import router from './server/routes';
 
-const { PORT = 3000 } = process.env;
+const { PORT } = process.env;
 
 const app = express();
 
@@ -10,6 +14,12 @@ app.use('/storybook', express.static('dist/storybook'));
 // Serve app production bundle
 app.use(express.static('dist/app'));
 
+app.use('/api', router);
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
+});
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
 });
