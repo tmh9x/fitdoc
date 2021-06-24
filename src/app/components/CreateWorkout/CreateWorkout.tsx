7ae31c3b0534226e3c928cf9b6a7a32916ee8cc7
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-24 10:40:45 +0200

    💄 Styling changes

--- src/app/components/CreateWorkout/CreateWorkout.tsx
+++ src/app/components/CreateWorkout/CreateWorkout.tsx
@@ -7,11 +7,13 @@ type CreateWorkoutProps = {
 
 function CreateWorkout({ name }: CreateWorkoutProps): JSX.Element {
   return (
-    <div className="grid grid-cols-2 p-2 rounded-xl bg-secondary w-80">
-      <div>{name}</div>
-      <div className="place-self-end">
-        <input className="w-10 mr-4 border rounded" type="number"></input>
-        <input className="w-4 h-4" type="checkbox"></input>
+    <div className="p-4 rounded-xl bg-secondary w-96">
+      <div className="grid grid-cols-2 p-2 mx-auto border rounded-xl bg-secondary w-80">
+        <div>{name}</div>
+        <div className="place-self-end">
+          <input className="w-10 mr-8 border rounded" type="number"></input>
+          <input className="w-4 h-4" type="checkbox"></input>
+        </div>
       </div>
     </div>
   );
