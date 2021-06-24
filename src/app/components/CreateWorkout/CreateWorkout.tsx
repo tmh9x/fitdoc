f49633f3c8764a6132cd43318eaa7c4636a58951
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-24 13:45:29 +0200

    💄 Fix CreateWorkout styling

--- src/app/components/CreateWorkout/CreateWorkout.tsx
+++ src/app/components/CreateWorkout/CreateWorkout.tsx
@@ -7,8 +7,15 @@ type CreateWorkoutProps = {
 
 function CreateWorkout({ name }: CreateWorkoutProps): JSX.Element {
   return (
-    <div className="p-4 rounded-xl bg-secondary w-96">
-      <div className="grid grid-cols-2 p-2 mx-auto border rounded-xl bg-secondary w-80">
+    <div className="py-4 mx-auto rounded-xl bg-secondary w-80">
+      <div className="mx-auto mb-4 w-72">
+        <input
+          type="text"
+          placeholder="Workoutname"
+          className="h-10 p-4 border w-72 rounded-xl"
+        ></input>
+      </div>
+      <div className="grid grid-cols-2 p-2 mx-auto border rounded-xl bg-secondary w-72">
         <div className="pl-2">{name}</div>
         <div className="place-self-end">
           <input className="w-10 mr-8 border rounded" type="number"></input>
