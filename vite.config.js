15e1a1a59c9ee4f6f6e4728fa6914c41ac6dab2a
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-25 11:29:14 +0200

    ✨ Add proxy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
+const BACKEND_PORT = process.env.PORT || 6000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
     outDir: 'dist/app',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${BACKEND_PORT}`,
+        changeOrigin: true,
+      },
+    },
   },
 });
