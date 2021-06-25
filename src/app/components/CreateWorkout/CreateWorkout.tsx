34059735d45b34cff6b5af70e0c7313afb782ba7
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-25 11:33:33 +0200

    ♻️ Fix code & display data from api

--- src/app/components/CreateWorkout/CreateWorkout.tsx
+++ src/app/components/CreateWorkout/CreateWorkout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
+import { Exercise } from '../../../server/wger';
 
 type CreateWorkoutProps = {
-  name: string;
+  exercises: Exercise[];
 };
 
-function CreateWorkout({ name }: CreateWorkoutProps): JSX.Element {
+function CreateWorkout({ exercises }: CreateWorkoutProps): JSX.Element {
   return (
     <div className="py-4 mx-auto rounded-xl bg-secondary w-80">
       <div className="mx-auto mb-4 w-72">
@@ -15,13 +16,18 @@ function CreateWorkout({ name }: CreateWorkoutProps): JSX.Element {
           className="h-10 p-4 border w-72 rounded-xl"
         ></input>
       </div>
-      <div className="grid grid-cols-2 p-2 mx-auto border rounded-xl bg-secondary w-72">
-        <div className="pl-2">{name}</div>
-        <div className="place-self-end">
-          <input className="w-10 mr-8 border rounded" type="number"></input>
-          <input className="w-4 h-4" type="checkbox"></input>
-        </div>
-      </div>
+      {exercises &&
+        exercises.map((exercise) => (
+          <div className="grid grid-cols-3 p-2 mx-auto mb-2 border last:mb-0 rounded-xl bg-secondary w-72">
+            <div className="col-start-1 col-end-3 pl-2 text-xs">
+              {exercise.name}
+            </div>
+            <div className="col-start-3 place-self-end">
+              <input className="w-10 mr-4 border rounded" type="number"></input>
+              <input className="w-4 h-4" type="checkbox"></input>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import 'tailwindcss/tailwind.css';
 import BackButton from '../../components/BackButton/BackButton';
 import ProfileButton from '../../components/ProfileButton/ProfileButton';
+import back from '../../assets/back.png';
+import profile from '../../assets/profile.png';
 
 type HeaderProps = {
   thumbnail: string;
@@ -17,14 +19,14 @@ function Header({
   return (
     <header className="grid grid-cols-3 pb-2 text-3xl border-b-2 text-secondary border-primary">
       <div className="self-center">
-        {isBackButtonVisible && <BackButton thumbnail="back.png" />}
+        {isBackButtonVisible && <BackButton thumbnail={back} />}
       </div>
       <div className="flex justify-center place-items-center">
         <img className="pr-1 w-7 h-7" src={thumbnail} />
         <span>fitdoc</span>
       </div>
       <div className="self-center justify-self-end">
-        {isProfileButtonVisible && <ProfileButton thumbnail="profile.png" />}
+        {isProfileButtonVisible && <ProfileButton thumbnail={profile} />}
       </div>
     </header>
   );
