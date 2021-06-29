a7742ea919be83dacfbdb9140e28c8e580116e52
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-29 13:46:40 +0200

    ♻️ Map data on Home

--- src/app/components/ExerciseCard/ExerciseCard.tsx
+++ src/app/components/ExerciseCard/ExerciseCard.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
+import { Exercise } from '../../../server/wger';
 
 type ExerciseCardProps = {
   thumbnail: string;
-  name: string;
-  category: string;
+  exercises: Exercise[];
 };
 
 function ExerciseCard({
   thumbnail,
-  name,
-  category,
+  exercises,
 }: ExerciseCardProps): JSX.Element {
   return (
-    <article className="grid w-32 text-sm bg-secondary h-44 rounded-xl">
-      <img
-        className="object-cover h-24 w-44 rounded-t-xl"
-        src={thumbnail}
-        alt=""
-      />
-      <h3 className="pl-1 font-bold">{name}</h3>
-      <div className="pl-1">{category}</div>
-    </article>
+    <>
+      {exercises &&
+        exercises.map((exercise) => (
+          <article className="grid flex-shrink-0 w-32 text-sm text-primary bg-secondary h-44 rounded-xl">
+            <img
+              className="object-cover h-24 w-44 rounded-t-xl"
+              src={thumbnail}
+            />
+            <h3 className="pl-1 text-xs font-bold">{exercise.name}</h3>
+            <div className="pl-1">{exercise.category}</div>
+          </article>
+        ))}
+    </>
   );
 }
 export default ExerciseCard;
--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -7,10 +7,17 @@ import logo from '../../assets/logo.png';
 import Button from '../../components/Button/Button';
 import pushups from '../../assets/pushups.jpeg';
 import { Link } from 'react-router-dom';
+import { Exercise } from '../../../server/wger';
+import useFetch from '../../hooks/useFetch';
 
 function Home(): JSX.Element {
+  const exercises = useFetch<Exercise[]>('/api/exercises');
+
+  if (!exercises) {
+    return <div>Not found!</div>;
+  }
   return (
-    <div className="m-6">
+    <div className="m-6 text-secondary">
       <header className="pt-2 bg-primary">
         <Header
           thumbnail={logo}
@@ -19,11 +26,11 @@ function Home(): JSX.Element {
         />
       </header>
 
-      <main className="text-secondary">
+      <main>
         <h2 className="pb-6 mt-6">Popular Workouts</h2>
         <div className="flex flex-row flex-shrink-0 gap-4 overflow-x-scroll">
           <div>
-            <WorkoutCardLarge thumbnail={pushups} name="#" />
+            <WorkoutCardLarge thumbnail={pushups} name="Push-Day" />
           </div>
           <div>
             <WorkoutCardLarge thumbnail={pushups} name="#" />
@@ -34,16 +41,8 @@ function Home(): JSX.Element {
         </div>
 
         <h2 className="pt-6 pb-6">All Exercises</h2>
-        <div className="flex flex-row flex-shrink-0 gap-4 overflow-x-scroll">
-          <div>
-            <ExerciseCard thumbnail={pushups} name="#" category="" />
-          </div>
-          <div>
-            <ExerciseCard thumbnail={pushups} name="#" category="" />
-          </div>
-          <div>
-            <ExerciseCard thumbnail={pushups} name="#" category="" />
-          </div>
+        <div className="flex flex-row gap-4 overflow-x-scroll">
+          <ExerciseCard thumbnail={pushups} exercises={exercises} />
         </div>
 
         <h2 className="pt-6">Create your personalized Workout!</h2>
