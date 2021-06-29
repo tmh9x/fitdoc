0e2550abb4b6c9384f7d145a6d24ae83331610e5
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-29 14:13:37 +0200

    💄 Extend Tailwind config grid-rows and fix styling

--- src/app/components/ExerciseCard/ExerciseCard.tsx
+++ src/app/components/ExerciseCard/ExerciseCard.tsx
@@ -15,11 +15,13 @@ function ExerciseCard({
     <>
       {exercises &&
         exercises.map((exercise) => (
-          <article className="grid flex-shrink-0 w-32 text-sm text-primary bg-secondary h-44 rounded-xl">
-            <img
-              className="object-cover h-24 w-44 rounded-t-xl"
-              src={thumbnail}
-            />
+          <article className="grid flex-shrink-0 w-32 grid-flow-row text-sm grid-rows-exerciseCard text-primary bg-secondary h-44 rounded-xl">
+            <div>
+              <img
+                className="object-cover h-24 w-44 rounded-t-xl"
+                src={thumbnail}
+              />
+            </div>
             <h3 className="pl-1 text-xs font-bold">{exercise.name}</h3>
             <div className="pl-1">{exercise.category}</div>
           </article>
