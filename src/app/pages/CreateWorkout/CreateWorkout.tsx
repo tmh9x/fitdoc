2a9318bd803c084b38bd0e917b9b04b8ac8d74f6
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-30 17:02:25 +0200

    ♻️ Refactor Button

--- src/app/pages/CreateWorkout/CreateWorkout.tsx
+++ src/app/pages/CreateWorkout/CreateWorkout.tsx
@@ -6,11 +6,11 @@ import Button from '../../components/Button/Button';
 import useFetch from '../../hooks/useFetch';
 import { Exercise } from '../../../server/wger';
 import logo from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function CreateWorkout(): JSX.Element {
   const exercises = useFetch<Exercise[]>('/api/exercises');
-
+  const history = useHistory();
   if (!exercises) {
     return <div>Not found!</div>;
   }
@@ -21,10 +21,10 @@ function CreateWorkout(): JSX.Element {
       <div className="mt-6">
         <CreateWorkoutCard exercises={exercises} />
       </div>
-      <div className="flex mt-4 place-content-center">
-        <Link to="./TrackWorkout">
-          <Button children="SAVE" variant="primary" />
-        </Link>
+      <div className="flex place-content-center">
+        <Button onClick={() => history.push('/track')} variant="primary">
+          save
+        </Button>
       </div>
     </div>
   );
